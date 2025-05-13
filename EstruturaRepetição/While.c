084e39b7cc99b53387205f3514ca1f5fd8fa4a77
Breno Alves <150525236+BrenoDx@users.noreply.github.com>
2025-05-12 22:42:57 -0300

    Fix: ATV while

---
+++ EstruturaRepetição/While.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+	int contador = 0;
+	printf("\nExemplo de estrututra de repetição while");
+	printf("---------------------------------------------");
+	
+	while(contador <10){
+		contador++;
+		printf("\n O valor do contador é = %d", contador);
+	}
+	return 0;
+}
