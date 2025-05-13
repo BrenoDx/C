d3518cad15086656c2fe81a544cd5bcab9ab9368
Breno Alves <150525236+BrenoDx@users.noreply.github.com>
2025-05-13 15:38:39 -0300

    Fix: ATV For

---
+++ EstruturaRepetição/For.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+	printf("\nExemplo de estrutura de repetição for");
+	printf("\n----------------------------------------------------");
+	for(int i = 1; i <= 10; i++){
+		printf("\n O valor do contador é = %d", i);
+	}
+	return 0;
+}
